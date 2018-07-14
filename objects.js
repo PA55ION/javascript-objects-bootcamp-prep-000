8e95f3f64c2c677fe14897f715647d3bde3eb8ac
Phakarat Campbell <jirallc@gmail.com>
2018-07-14 17:31:15 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,8 +4,8 @@ function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign({}, playlist, { [artistName]: 'songTitle'});
 }
 updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi")
-//
-// function removeFromPlaylist(playlist, artistName) {
-//   delete playlist.artistName;
-//
-// }
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName;
+
+}
