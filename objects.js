c5676c2410c5829f18b92e8acb601a9b59ac5b84
Phakarat Campbell <jirallc@gmail.com>
2018-07-14 18:02:05 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = { artistName: 'songTitle' }
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  return Object.assign({}, playlist, { [artistName]: 'songTitle'});
+  return Object.assign(playlist, { [artistName]: 'songTitle'});
 }
 updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi")
 
