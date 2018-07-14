cf8e8e6f959ebfae60cd6ffabdd79274c3f8f979
Phakarat Campbell <jirallc@gmail.com>
2018-07-14 18:14:54 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,7 +6,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
 updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi")
 
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist.artistName;
+  delete playlist.Slowdive;
   return playlist;
 
 }
